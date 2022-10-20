18e85be6dd901227130f89f10d4f98fe14550702
Maciel Eustaquio <maciel.seo@gmail.com>
2022-10-20 11:22:24 -0300

    [ME] Finaliza a parte 4 do projeto. Cria o controlador dos elementos HTML.

---
+++ etapas.js
@@ -0,0 +1,74 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Fábio Rogério',
+                partido: 'PT',
+                fotos: [
+                    {
+                        url: '38111.jpg',
+                        legenda: 'Vereador',
+                        small: false
+                    }
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Mario Chalis',
+                partido: 'SEM PARTIDO',
+                fotos: [
+                    {
+                        url: '77222.jpg',
+                        legenda: 'Vereador',
+                        small: false
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Rômulo Silva',
+                partido: 'PL',
+                vice: 'Vagner Barbosa',
+                fotos: [
+                    {
+                        url: '99.jpg',
+                        legenda: 'Prefeito',
+                        small: false
+                    },
+                    {
+                        url: '99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: 87,
+                name: 'Guilherme Chaves',
+                partido: 'PC',
+                vice: 'Henry Castro',
+                fotos: [
+                    {
+                        url: '84.jpg',
+                        legenda: 'Prefeito',
+                        small: false
+                    },
+                    {
+                        url: '84_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+        ]
+    }
+];
